c76b5ea7511203de66791064cde105f129a6a1fc
Victoria B <viktorija.bourassa@gmail.com>
2019-04-24 20:35:26 +0000

    Automatically backed up by Learn

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,7 +1,7 @@
 class DoctorsController < ApplicationController
-  def index
-    @doctors = Doctor.all
-  end
+  # def index
+  #   @doctors = Doctor.all
+  # end
   def show
     @doctor = Doctor.find(params[:id])
     @appointments = @doctor.appointments
