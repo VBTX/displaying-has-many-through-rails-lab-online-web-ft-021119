2dcfe96361c8051d478a5a5cca977b568bdb9976
Victoria B <viktorija.bourassa@gmail.com>
2019-04-24 19:17:41 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190424191420) do
+ActiveRecord::Schema.define(version: 20190424191731) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
